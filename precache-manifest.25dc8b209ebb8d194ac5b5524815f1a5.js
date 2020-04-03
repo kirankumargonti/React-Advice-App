14f87615449616e49388ec7ce3bbb363c1ae05a1
Kirankumar Gonti <kirankumar225rk@gmail.com>
2020-04-03 10:52:55 +0530

    Updates

---
+++ precache-manifest.25dc8b209ebb8d194ac5b5524815f1a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400c7634491dff73f2b5ce16306448e7",
+    "url": "/React-Advice-App/index.html"
+  },
+  {
+    "revision": "a84520aefb38d13035d6",
+    "url": "/React-Advice-App/static/css/main.014e2dcb.chunk.css"
+  },
+  {
+    "revision": "281267003612acedcb86",
+    "url": "/React-Advice-App/static/js/2.75b2f9cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Advice-App/static/js/2.75b2f9cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84520aefb38d13035d6",
+    "url": "/React-Advice-App/static/js/main.7d3a6c62.chunk.js"
+  },
+  {
+    "revision": "4e1a4bf9623b3cbd7ecf",
+    "url": "/React-Advice-App/static/js/runtime-main.c9e5b8a6.js"
+  }
+]);
